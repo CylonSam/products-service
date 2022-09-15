23d2f05d877e86c346f3fb602707224ea5d8deb0
Samantha L. A. Silva <samantha.athayde@b2wdigital.com>
2022-09-15 18:36:42 -0300

    Remove arquivos desnecessários

--- src/test/java/com/americanas/productsservice/productsserviceTest/ProductServiceTest.java
+++ src/test/java/com/americanas/productsservice/productsserviceTest/ProductServiceTest.java
@@ -1,25 +1,16 @@
 package com.americanas.productsservice.productsserviceTest;
 
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
+import com.americanas.productsservice.AbstractTest;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@ContextConfiguration
-public class ProductServiceTest {
+public class ProductServiceTest extends AbstractTest {
 
-    //@Autowired
-   // private FindProductId serviceImpl;
+    // @Autowired
+    // private FindProductId serviceImpl;
 
     @Test
-    public void findProductByID(){
-
+    public void findProductByID() {
 
     }
 
